301efdd3078c62d6c0f4a9f3f88be110702166b9
SuminJN <21900635@handong.edu>
2022-01-14 12:48:36 +0900

    2022/01/14

---
+++ Baekjoon_Algorithm/1753_최단경로.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define MAX 20001
+#define INF 987654321
+
+int V, E;
+int start;
+int dist[MAX];
+vector< pair<int,int> > v[MAX];
+
+void Dijkstra()
+{
+    priority_queue< pair<int,int> > pq;
+
+    pq.push(make_pair(0, start));
+    dist[start] = 0;
+
+    while(!pq.empty()){
+        int cost = -pq.top().first;
+        int current = pq.top().second;
+        pq.pop();
+
+        if(dist[current] < cost)
+            continue;
+        
+        for(int i = 0; i < v[current].size(); i++){
+            int next_cost = v[current][i].second + cost;
+            int next = v[current][i].first;
+
+            if(dist[next] > next_cost){
+                dist[next] = next_cost;
+                pq.push(make_pair(-next_cost, next));
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> V >> E;
+    cin >> start;
+
+    int a, b, c;
+    for(int i = 0; i < E; i++){
+        cin >> a >> b >> c;
+        v[a].push_back(make_pair(b, c));
+    }
+
+    for(int i = 1; i <= V; i++){
+        dist[i] = INF;
+    }
+
+    Dijkstra();
+
+    for(int i = 1; i <= V; i++){
+        if(dist[i] == INF)
+            cout << "INF" << "\n";
+        else 
+            cout << dist[i] << "\n";
+    }
+
+    return 0;
+}
