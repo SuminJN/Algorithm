1c46e841427a5ee58de754e22132b022df6cb0a5
Sumin Jeon <71697350+SuminJN@users.noreply.github.com>
2023-12-28 11:41:35 +0900

    Java / 6.099166666666668ms

---
+++ PROGRAMMERS/소수 찾기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.stream.Collectors;
+​
+class Solution {
+    
+    Set<Integer> set;
+    String[] part;
+    boolean[] visited;
+    
+    public int solution(String numbers) {
+        set = new HashSet<>();
+        part = numbers.split("");
+        visited = new boolean[numbers.length()];
+        
+        permutation("");
+        
+        return set.stream()
+            .filter(number -> isPrime(number))
+            .collect(Collectors.toList())
+            .size();
+    }
+    
+    private void permutation(String str) {
+        if(!str.isEmpty()) {
+            set.add(Integer.parseInt(str));
+        }
+        
+        for(int i = 0; i < part.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                permutation(str + part[i]);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    private boolean isPrime(int number) {
+        if(number < 2) return false;
