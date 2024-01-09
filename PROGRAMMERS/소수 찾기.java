a3ccb56473af8171d296ff1047f153ca28a85ffc
Sumin Jeon <71697350+SuminJN@users.noreply.github.com>
2024-01-09 14:39:50 +0900

    Java / 6.099166666666668ms

--- PROGRAMMERS/소수 찾기.java
+++ PROGRAMMERS/소수 찾기.java
@@ -36,3 +36,11 @@ class Solution {
     
     private boolean isPrime(int number) {
         if(number < 2) return false;
+        
+        for(int i = 2; i*i <= number; i++) {
+            if(number % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
