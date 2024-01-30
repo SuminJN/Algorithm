d1baa5cbfc6a04954e6586223da7a91e12c1c1bd
Sumin Jeon <71697350+SuminJN@users.noreply.github.com>
2024-01-30 10:21:58 +0900

    Java / 66.02199999999999ms

---
+++ PROGRAMMERS/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+​
+class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String n1, String n2) {
+                return (n2 + n1).compareTo(n1 + n2);
+            }
+        });
+        
+        if(arr[0].equals("0")) return "0";
+        
+        String answer = String.join("", arr);
+        
+        return answer;
+    }
+}
+                return (n2 + n1).compareTo(n1 + n2);
