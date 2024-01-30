ae6cc6108fd4d628b1ee94c00ff62df41476afe9
Sumin Jeon <71697350+SuminJN@users.noreply.github.com>
2024-01-30 09:33:48 +0900

    Java / 4.765714285714286ms

---
+++ PROGRAMMERS/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+​
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            
+            Arrays.sort(arr);
+            
+            answer.add(arr[commands[i][2]-1]);
+        }
+        
+        System.out.println(answer);
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
