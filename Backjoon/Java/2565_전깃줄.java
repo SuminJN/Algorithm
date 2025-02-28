036db54a7cad8d0532dc31f66da12864534c12f5
SuminJN <wjstnals1211@gmail.com>
2025-02-28 13:26:39 +0900

    2025.02.28

---
+++ Backjoon/Java/2565_전깃줄.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        Arrays.sort(arr, ((o1, o2) -> o1[0] - o2[0]));
+
+        int[] dp = new int[N];
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N - max);
+    }
+}
