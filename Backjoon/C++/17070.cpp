c1076d5701947da6d4be7412ede5af533b036df8
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-13 23:51:50 +0900

    2025.05.13

---
+++ Backjoon/C++/17070.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a[20][20], dp[20][20][3];
+
+bool check(int y, int x, int d) {
+    if (d == 0 || d == 2) {
+        if (a[y][x] == 0) return true;
+    } else {
+        if (a[y][x] == 0 && a[y - 1][x] == 0 && a[y][x - 1] == 0) return true;
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    dp[1][2][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (check(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][0];
+            if (check(i + 1, j + 1, 1)) dp[i + 1][j + 1][1] += dp[i][j][0];
+
+            if (check(i + 1, j, 2)) dp[i + 1][j][2] += dp[i][j][2];
+            if (check(i + 1, j + 1, 1)) dp[i + 1][j + 1][1] += dp[i][j][2];
+
+            if (check(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][1];
+            if (check(i + 1, j + 1, 1)) dp[i + 1][j + 1][1] += dp[i][j][1];
+            if (check(i + 1, j, 2)) dp[i + 1][j][2] += dp[i][j][1];
+        }
+    }
+
+    int answer = dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
+
+    cout << answer;
+
+    return 0;
+}
