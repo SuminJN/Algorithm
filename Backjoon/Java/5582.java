579464e1bfd8a3ca368d55a9a8842241eadada9a
Sumin Jeon <wjstnals1211@gmail.com>
2025-08-05 14:46:05 +0900

    2025.08.05

---
+++ Backjoon/Java/5582.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int[][] lcs = new int[A.length() + 1][B.length() + 1];
+        int max = 0;
+
+        for (int i = 1; i <= A.length(); i++) {
+            for (int j = 1; j <= B.length(); j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                    max = Math.max(max, lcs[i][j]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
