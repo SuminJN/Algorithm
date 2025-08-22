c3629f17669a9ed9853e471d74322f42c3386a27
Sumin Jeon <wjstnals1211@gmail.com>
2025-08-22 12:50:40 +0900

    2025.08.22

---
+++ Backjoon/Java/16395.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][M + 1];
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (j == 1 || i == j) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N][M]);
+    }
+}
