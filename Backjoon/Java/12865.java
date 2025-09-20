eefaf89fa09c0bfd548c109364c1e73a3276b48e
Sumin Jeon <wjstnals1211@gmail.com>
2025-09-20 20:46:52 +0900

    2025.09.20

---
+++ Backjoon/Java/12865.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static int[] w, v;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        w = new int[N + 1];
+        v = new int[N + 1];
+        dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (j - w[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
