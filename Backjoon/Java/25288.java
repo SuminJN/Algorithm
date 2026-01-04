84529214c7ec9a211650cfe1afe3767295083d14
Sumin Jeon <wjstnals1211@gmail.com>
2026-01-04 20:58:58 +0900

    2026.01.04

---
+++ Backjoon/Java/25288.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        while(N --> 0) {
+            System.out.print(str);
+        }
+
+    }
+}
